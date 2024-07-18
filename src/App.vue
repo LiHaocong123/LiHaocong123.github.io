eae19b3a3a51927345775a11f0eaa965533f6479
LiHaocong123 <m13120077475@163.com>
2024-07-18 14:31:55 +0800

    QA

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+// import QA from './components/QA.vue';
+import TextPage from './views/TextPage.vue';
+export default {
+  name: 'App',
+  components: {
+    TextPage
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
